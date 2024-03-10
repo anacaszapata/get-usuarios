b6056ef82b49016921135a5ad3812486152c5b1c
Ana Sofia Castrillòn <149738059+anacaszapata@users.noreply.github.com>
2024-03-10 17:52:06 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,61 +1,116 @@
-
 // 2)Listado de 10 usuarios
- const express= require('express');
- const mongoose= require('mongoose');
+const express= require('express');
+const mongoose= require('mongoose');
 
- mongoose.connect('mongodb+srv://anacaszapata:5tSP9p7hmKVKViOV@pruebas.jplyxw5.mongodb.net/')
- const db=mongoose.connection;
+mongoose.connect('mongodb+srv://anacaszapata:Ng0Rys50wAVCaxOm@cluster0.tys510v.mongodb.net/')
+const db=mongoose.connection;
 
- db.on('error',console.error.bind(console,'connection error'));
+db.on('error',console.error.bind(console,'connection error'));
 
 db.once('open', function(){
-   console.log('Connected to MongoDB');
-    //   model
-     userSchema = mongoose.Schema({
-         nombres:String,
-         apellidos:String
-     });
-     empresaSchema = mongoose.Schema({
-        nombre:String,
-        nit:String
+  console.log('Connected to MongoDB');
+   //   model
+    userSchema = mongoose.Schema({
+        nombres:String,
+        apellidos:String
     });
     empresaSchema = mongoose.Schema({
-        nombre:String,
-        nit:String
-    });
+       nombre:String,
+       nit:String
+   });
+
+usersSchema = mongoose.Schema({
+    nombres: String,
+    apellidos: String,
+    empresa_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Empresa' }
+});
+
+
+ userioSchema = mongoose.Schema({
+    nombres: String,
+    apellidos: String,
+    pais: String  
+});
+
+ empresasSchema = mongoose.Schema({
+    nombre: String,
+    ciudad: String 
+});
+
+
+   
+   //  2)listado de 10 usuarios
+   const User = mongoose.model('User' ,userSchema);
+
+   // 3)listado de todas las empresas
+   const empresas = mongoose.model('empresas' ,empresaSchema);
+
+//    4)Listado de usuarios que sean de la empresa id5
+    const Usuario = mongoose.model('Usuario', usersSchema);
+
+    // 5)Listado de usuarios que sean de Bangladesh
+    const pais = mongoose.model('pais', userioSchema);
+
+    // 6)empresas de la ciudad bangladesh
+     const Empresa = mongoose.model('Empresa', empresasSchema);
 
     
-    //  2)listado de 10 usuarios
-    const User = mongoose.model('User' ,userSchema);
-
-    // 3)listado de todas las empresas
-    const empresas = mongoose.model('empresas' ,empresaSchema);
-
- const app = express();
- app.use(express.json());
 
 
-    // 2)listado de 10 usuarios
-  app.get('/api/users/limit',async(req,res)=>{
-  const users = await User.find().limit(10);
-  res.json(users);
-  });
-  
-    // 3)listado de todas las empresas
-  app.get('/api/companies',async(req,res)=>{
-    const empresa = await empresas.find();
-    res.json(empresa);
-    });
+const app = express();
+app.use(express.json());
 
-    // 4)Listado de usuarios que sean de la empresa id 5
-    app.get('api/users/companies/5',async(req,res)=>{
-    const usersempresa = await usersempresa.find()
-    })
 
- app.listen(3000,function(){
-     console.log('Servidor escuchando en el puerto 3000');
+   // 2)listado de 10 usuarios
+ app.get('/api/users/limit/:limit',async(req,res)=>{
+ const users = await User.find().limit(req.params.limit);
+ res.json(users);
  });
- })
+ 
+   // 3)listado de todas las empresas
+ app.get('/api/companies',async(req,res)=>{
+   const empresa = await empresas.find();
+   res.json(empresa);
+   });
+
+   // 4)Listado de usuarios que sean de la empresa id 5
+ app.get('/api/users/companies/:id', async (req, res) => {
+    console.log(req.params.id);
+    try {
+        const users_empresa = await empresas.find({ id: req.params.id });
+        res.json(users_empresa);
+    } catch {
+    }
+});
+//  5)Usuarios que sean de bangladesh
+app.get('/api/users/country/Bangladesh', async (req, res) => {
+    try {
+        const usersBangladesh = await pais.find({ nacionalidad: 'Bangladesh' });
+        res.json(usersBangladesh);
+    } catch (error) {
+    }
+});
+
+// 6)empresas que sean de la ciudad bangladesh
+app.get('/api/companies/city/Bangladesh', async (req, res) => {
+    try {
+        const empresasBangladesh = await Empresa.find({ ciudad: 'Bangladesh' });
+        res.json(empresasBangladesh);
+    } catch (error) {
+    }
+});
+
+
+
+
+  
+app.listen(3000,function(){
+    console.log('Servidor escuchando en el puerto 3000');
+});
+})
+
+  
+  
 
    
    
